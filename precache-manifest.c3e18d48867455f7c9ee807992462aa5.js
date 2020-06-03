5f1646bcfa04b638f5a1914c2185a85dc46485ab
Alireza Bagheri <alireza7394@gmail.com>
2020-06-03 04:57:37 +0430

    Updates

---
+++ precache-manifest.c3e18d48867455f7c9ee807992462aa5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca96bf3a4d63ff8266945eab36476be",
+    "url": "/yoello/index.html"
+  },
+  {
+    "revision": "09d50854d668477181f0",
+    "url": "/yoello/static/css/main.c032afef.chunk.css"
+  },
+  {
+    "revision": "17b76a8370189972c057",
+    "url": "/yoello/static/js/2.08820225.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/yoello/static/js/2.08820225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09d50854d668477181f0",
+    "url": "/yoello/static/js/main.728c3433.chunk.js"
+  },
+  {
+    "revision": "4f966ea51f227783c630",
+    "url": "/yoello/static/js/runtime-main.10c452cf.js"
+  },
+  {
+    "revision": "64ca5b5ad6d52e9449d6881908918564",
+    "url": "/yoello/static/media/cart.64ca5b5a.svg"
+  },
+  {
+    "revision": "23a33f9b57481082f9650c202b118d05",
+    "url": "/yoello/static/media/coffee.23a33f9b.svg"
+  },
+  {
+    "revision": "665926e22b76aa6339da4f7eab12c067",
+    "url": "/yoello/static/media/delete.665926e2.svg"
+  },
+  {
+    "revision": "7ba705fda5d2decb523204d25d9565bd",
+    "url": "/yoello/static/media/discout.7ba705fd.svg"
+  },
+  {
+    "revision": "c412776699c042548bfc30e7b2fa85ca",
+    "url": "/yoello/static/media/search.c4127766.svg"
+  },
+  {
+    "revision": "44dbaa78fb2ed240792c9e7a424b0ea0",
+    "url": "/yoello/static/media/spoon-knife.44dbaa78.svg"
+  }
+]);
